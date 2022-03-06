73ad9a98be18066fcd298ba229f4041a40e2e7ec
DigitalCyan <marinoljubetic8@gmail.com>
2022-03-06 06:08:14 +0100

    Init commit

---
+++ src/lib.rs
@@ -0,0 +1,165 @@
+mod vector3;
+
+#[cfg(test)]
+mod tests {
+    use std::f64::consts::PI;
+
+    use crate::vector3::Vector3;
+
+    #[test]
+    fn add() {
+        let a = Vector3 {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+        };
+        let b = Vector3 {
+            x: 2.0,
+            y: 3.0,
+            z: 4.0,
+        };
+
+        assert_eq!(
+            a + b,
+            Vector3 {
+                x: 5.0,
+                y: 5.0,
+                z: 5.0
+            }
+        );
+    }
+
+    #[test]
+    fn sub() {
+        let a = Vector3 {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+        };
+        let b = Vector3 {
+            x: 2.0,
+            y: 3.0,
+            z: 4.0,
+        };
+
+        assert_eq!(
+            a - b,
+            Vector3 {
+                x: 1.0,
+                y: -1.0,
+                z: -3.0
+            }
+        );
+    }
+
+    #[test]
+    fn mul() {
+        let a = Vector3 {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+        };
+
+        assert_eq!(
+            a * 5.0,
+            Vector3 {
+                x: 15.0,
+                y: 10.0,
+                z: 5.0
+            }
+        )
+    }
+
+    #[test]
+    fn div() {
+        let a = Vector3 {
+            x: 5.0,
+            y: 15.0,
+            z: 30.0,
+        };
+
+        assert_eq!(
+            a / 5.0,
+            Vector3 {
+                x: 1.0,
+                y: 3.0,
+                z: 6.0
+            }
+        )
+    }
+
+    #[test]
+    fn magnitude() {
+        let a = Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 3.0,
+        };
+
+        assert_eq!(a.magnitude(), 3.0);
+    }
+
+    #[test]
+    fn dot() {
+        let a = Vector3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+
+        let b = Vector3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+
+        assert_eq!(a.dot(&b), 14.0);
+    }
+
+    #[test]
+    fn cross() {
+        let a = Vector3 {
+            x: 1.0,
+            y: 2.0,
+            z: 3.0,
+        };
+
+        let b = Vector3 {
+            x: 3.0,
+            y: 2.0,
+            z: 1.0,
+        };
+
+        assert_eq!(
+            a.cross(&b),
+            Vector3 {
+                x: -4.0,
+                y: 8.0,
+                z: -4.0
+            }
+        );
+    }
+
+    #[test]
+    fn angle() {
+        let a = Vector3::from_coords(1.0, 0.0, 0.0);
+        let b = Vector3::from_coords(0.0, 0.0, 1.0);
+
+        assert_eq!(a.angle(b) * (180.0 / PI), 90.0);
+    }
+
+    #[test]
+    fn angle_deg() {
+        let a = Vector3::from_coords(1.0, 0.0, 0.0);
+        let b = Vector3::from_coords(0.0, 0.0, 1.0);
+
+        assert_eq!(a.angle_deg(b), 90.0);
+    }
+
+    #[test]
+    fn normalize() {
+        let a = Vector3::from_coords(0.5, 0.0, 0.0);
+
+        assert_eq!(a.normalize(), Vector3::from_coords(1.0, 0.0, 0.0));
+    }
+}
---
+++ src/vector3.rs
@@ -0,0 +1,119 @@
+use std::{
+    f64::consts::PI,
+    ops::{Add, Div, Mul, Sub},
+};
+
+#[derive(Debug)]
+pub struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vector3 {
+    pub fn new() -> Vector3 {
+        Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    }
+
+    pub fn from_coords(x: f64, y: f64, z: f64) -> Vector3 {
+        Vector3 { x, y, z }
+    }
+}
+
+impl Clone for Vector3 {
+    fn clone(&self) -> Self {
+        Vector3 {
+            x: self.x,
+            y: self.y,
+            z: self.z,
+        }
+    }
+}
+
+impl Add for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Sub for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vector3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, scalar: f64) -> Self::Output {
+        Vector3 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+            z: self.z * scalar,
+        }
+    }
+}
+
+impl Div<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, scalar: f64) -> Self::Output {
+        Vector3 {
+            x: self.x / scalar,
+            y: self.y / scalar,
+            z: self.z / scalar,
+        }
+    }
+}
+
+impl PartialEq for Vector3 {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y && self.z == other.z
+    }
+}
+
+impl Vector3 {
+    pub fn magnitude(&self) -> f64 {
+        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+    }
+
+    pub fn dot(&self, other: &Vector3) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn cross(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.y * other.z - other.y * self.z,
+            y: -(self.x * other.z - other.x * self.z),
+            z: self.x * other.y - other.x * self.y,
+        }
+    }
+
+    pub fn angle(&self, other: Vector3) -> f64 {
+        (self.dot(&other) / (self.magnitude() * other.magnitude())).acos()
+    }
+
+    pub fn angle_deg(&self, other: Vector3) -> f64 {
+        (self.dot(&other) / (self.magnitude() * other.magnitude())).acos() * (180.0 / PI)
+    }
+
+    pub fn normalize(&self) -> Vector3 {
+        self.clone() / self.magnitude()
+    }
+}
