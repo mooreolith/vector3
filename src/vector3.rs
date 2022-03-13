8b97e083ce4da41fab70cace633c36def9a1d2b1
DigitalCyan <marinoljubetic8@gmail.com>
2022-03-13 20:12:26 +0100

    Allowed dead code within the vector3 module

--- src/vector3.rs
+++ src/vector3.rs
@@ -10,6 +10,7 @@ pub struct Vector3 {
     pub z: f64,
 }
 
+#[allow(dead_code)]
 impl Vector3 {
     pub fn new(x: f64, y: f64, z: f64) -> Vector3 {
         Vector3 { x, y, z }
@@ -94,6 +95,7 @@ impl PartialEq for Vector3 {
     }
 }
 
+#[allow(dead_code)]
 impl Vector3 {
     pub fn magnitude(&self) -> f64 {
         (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
